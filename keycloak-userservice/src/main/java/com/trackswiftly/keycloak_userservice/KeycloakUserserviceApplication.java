7f9cad7c0ecb9914545b3527b35d42b824cf2d9f
Abdellatif Hour <abdellatif.hour.web@gmail.com>
2025-01-29 00:07:04 +0100

    Add keycloak-userservice a custom rest api

---
+++ keycloak-userservice/src/main/java/com/trackswiftly/keycloak_userservice/KeycloakUserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.trackswiftly.keycloak_userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakUserserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakUserserviceApplication.class, args);
+	}
+
+}
---
+++ keycloak-userservice/src/test/java/com/trackswiftly/keycloak_userservice/KeycloakUserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackswiftly.keycloak_userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakUserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
